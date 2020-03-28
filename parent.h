ee1c3fedb91422b84fbb48ce762a2d61fcaf3f7e
ranaaa220 <55308507+ranaaa220@users.noreply.github.com>
2020-03-28 19:33:46 +0700

    update parent.h

--- parent.h
+++ parent.h
@@ -1,5 +1,38 @@
 #ifndef PARENT_H_INCLUDED
 #define PARENT_H_INCLUDED
+#include <iostream>
+#define info(A) A->info
+#define next(A) A->next
+#define prev(A) A->prev
+#define first(A) A.first
+#define last(A) A.last
 
+using namespace std;
+
+struct pembeli{
+    int id;
+    string nama;
+    float saldo;
+};
+
+typedef struct elmList *address;
+
+struct elmList{
+    pembeli info;
+    address next;
+    address prev;
+};
+
+struct list{
+    address first;
+    address last;
+};
+
+void buatlist(list &l);
+bool listkosong(list l);
+void tambahdatapertama(list &l,address p);
+void hapusdataterakhir(list &l,address &p);
+void tambahdatasetelah(list &l,address p,address prec)
+void tampildata(list l);
 
 #endif // PARENT_H_INCLUDED
