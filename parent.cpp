a411d60c3335ffd6d959470457f59cc0ee04f460
Daffa Hilmy Fadhlurrohman <55197547+DaffaHilmyF@users.noreply.github.com>
2020-03-28 19:19:36 +0700

    upload parent dan relasi

---
+++ parent.cpp
@@ -0,0 +1,19 @@
+void printChildList(listChild L){
+    address P = first(L);
+    if(listChildIsEmpty(L) == true){
+        cout << "Maaf tidak ada barang" << endl;
+    }else{
+        cout << endl;
+        while(next(P) != first(L)){
+            cout <<"ID          : " << info(P).id_Barang << endl;
+            cout <<"Nama Barang : " << info(P).nama_Barang << endl;
+            cout <<"Stock       : " << info(P).stock_Barang << endl;
+            cout <<"Harga       : " << info(P).harga_Barang << endl;
+            P = next(P);
+        }
+            cout <<"ID          : " << info(P).id_Barang << endl;
+            cout <<"Nama Barang : " << info(P).nama_Barang << endl;
+            cout <<"Stock       : " << info(P).stock_Barang << endl;
+            cout <<"Harga       : " << info(P).harga_Barang << endl;
+    }
+}
