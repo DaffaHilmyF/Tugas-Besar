1d47cc0c1d7926cd1e864b9617f360e296a371f6
ranaaa220 <55308507+ranaaa220@users.noreply.github.com>
2020-03-30 16:33:44 +0700

    update parent.cpp
    daffa, tolong cek create elmnya aku pakenya inputan

--- parent.cpp
+++ parent.cpp
@@ -1,65 +1,66 @@
 #include "parent.h"
 
-void buatlist(list &l) {
-    first(l) = NULL;
-    last(l) = NULL;
+void createList(List_parent &L) {
+    first(L) = NULL;
+    last(L) = NULL;
 }
 
-bool listkosong(list l) {
-    if (first(l) == NULL && last(l) == NULL) {
+bool listParentEmpty(List_parent L) {
+    if (first(L) == NULL && last(L) == NULL) {
         return true;
     } else {
         return false;
     }
 }
 
-void createElement(address &p, string nama, int id, float saldo) {
-    p = new elmList;
+void createParentElmt(address_parent &P, int customerID, string customerName, int saldo) {
+    P = new elmList_parent;
     
-    cout<<"input nama pelanggan"<<endl;
+    cout<<"masukkan nama pelanggan"<<endl;
     cin>> nama;
-    info(p).nama = nama;
+    info(p).nama = customerName;
     cout<< endl;
     
-    cout<<"input id pelanggan"<<endl;
+    cout<<"masukkan ID pelanggan"<<endl;
     cin>> id;
-    info(p).id = id;
+    info(p).id = customerID;
     cout<< endl;
     
-    cout<<"input saldo pelanggan"<<endl;
+    cout<<"masukkan saldo pelanggan"<<endl;
     cin>> saldo;
-    info(p).id = saldo;
+    info(p).saldo = saldo;
     cout<< endl;
     
     next(p) = NULL;
     prev(p) = NULL;
 };
 
-void tambahdatapertama(list &l,address p) {
-    if(listkosong(l)==true) {
-        first(l)= p;
-        last(l) = p;
+void insertFirstParent(List_parent &L, address_parent P) {
+    address_parent Q;
+    if(listParentEmpty(L)==true) {
+        first(L)= P;
+        last(L) = P;
     } else {
-        next(p) = first(l);
-        prev(first(l)) = p;
-        first(l) = p;
+        next(P) = first(L);
+        prev(first(L)) = P;
+        first(L) = P;
     }
 }
 
-void hapusdataterakhir(list &l,address &p) {
-    if (listkosong(l) == true) {
+void deleteLastParent(List_parent &L, address_parent &P){
+    if (listParentEmpty(L) == true) {
         cout << "data Kosong";
         cout << endl;
-        p = NULL;
-    } else if (next(first(l)) == NULL && next(last(l)) == NULL) {
-        p = first(l);
-        first(l) = NULL;
-        last(l) = NULL;
+        P = NULL;
+    } else if (next(first(L)) == NULL && next(last(L)) == NULL) {
+        P = first(L);
+        first(L) = NULL;
+        last(L) = NULL;
     } else {
-        p = last(l);
-        last(l) = prev(p);
-        prev(p) = NULL;
-        next(last(l)) = NULL;
+        P = last(L);
+        last(L) = prev(P);
+        prev(P) = NULL;
+        next(last(L)) = NULL;
     }
 }
 
@@ -77,13 +78,23 @@ void tambahdatasetelah(list &l,address p,address prec){
     }
 }
 
-void tampildata(list l) {
-    address p;
-    p = first(l);
-    while (p != NULL ) {
-        cout << info(p)<< " " ;
-        p = next(p);
+void printInfo(List_parent L) {
+    address_parent P = first(L);
+    P = first(L);
+    while (P != NULL ) {
+        cout << info(P).id << info(P).nama << info(P).saldo << endl;
+        P = next(P);
     }
-
     cout << endl;
+}
+
+address_parent findElmParent(List_parent L, string X) {
+    address_parent P = first(L);
+    while(P != first(L)){
+        if(info(P).nama == X) {
+            return P;
+        }
+        P = next(P);
+    }
+    return NULL;
 }
