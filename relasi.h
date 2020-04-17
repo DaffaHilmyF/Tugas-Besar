1e9f09c6062cff1d503c484559cb971379ba6cca
Daffa Hilmy Fadhlurrohman <55197547+DaffaHilmyF@users.noreply.github.com>
2020-04-18 00:36:50 +0700

    Update relasi.h

--- relasi.h
+++ relasi.h
@@ -7,6 +7,7 @@
 #include "parent.h"
 
 
+
 #define next(P) P->next
 #define first(L) L.first
 #define child(P) P->child
@@ -36,5 +37,7 @@ address_relasi findElmRelasi(List_relasi L, address_parent P, address_child C);
 void printInfoRelasi(List_relasi L) ;
 void showRelationParent(List_relasi R, List_child C, List_parent P);
 void showRelationChild(List_relasi R, List_child C, List_parent P);
+
+
 
 #endif // RELASI_H_INCLUDED
